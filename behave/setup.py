81e5f6b772bd7cacde73b2de92c5481bfe5597b0
Jack Muratore <jackmuratore@gmail.com>
2018-04-07 23:10:04 -0400

    rename behave stuff

--- behave/setup.py
+++ behave/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 setup(
-    name='test-demo',
+    name='jacks-behave-demo',
     version='0.0.1',
     description='a project',
     packages=find_packages(exclude=[]),
     install_requires=[
         'behave',
-        # 'sarge'
         ],
     data_files=[],
     entry_points={
